12a6d92fcc8b005e5a6cfde0c1e57568a0519665
Kate Rivera <kateashley.rivera@ucalgary.ca>
2022-01-27 17:17:44 -0700

    Assignment 1 Skeleton

---
+++ javascript/logic.js
@@ -0,0 +1,22 @@
+// Code credits: this code was written by Dr. Pavol Federl for the SENG 513 course at the UofC
+// https://codepen.io/pfederl/pen/JEMKwB
+
+
+function getStats(txt) {
+    // you need to write your own code here
+
+   
+   
+
+    return {
+        nChars: 58,                                                     
+        nWords: 22,
+        nLines: 10,
+        nNonEmptyLines: 22,
+        averageWordLength: 3.3,
+        maxLineLength: 33,
+        tenLongestWords: ["xxxxxxxxx", "123444444"],
+        tenMostFrequentWords: ["a", "this", "the"]
+    };
+
+}
